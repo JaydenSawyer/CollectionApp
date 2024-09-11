337b423aca86c60e172f244f64562f24d4601447
Jayden Sawyer <jayden.sawyer25@d155.org>
2024-09-11 07:55:14 -0500

    initial commit

--- CollectionApp/ViewController.swift
+++ CollectionApp/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // #1
+    var cool: [String: [String]] = [:]
+ 
+    var jo: [Int] = []
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       //#2
+        jo.append(5)
+        jo.append(2)
+       //#3
+        print(jo)
+        //#4
+        jo.insert(3, at: 1)
+        //#5
+        var i = 0
+        while i < jo.count {
+            print(jo[1])
+            i += 1
+        }
+        //#6
+        print(jo)
+        jo.sort(by: >)
+        for i in 0..<jo.count{
+            print(jo[i])
+        }
+        cool["C"] = ["Crystal Lake Central"]
+        cool["S"] = ["Crystal Lake South"]
+        cool["P"] = ["PR"]
+        cool["CG"] = ["Cary Grove"]
+        print(cool)
+        }
+    
     }
+    
 
 
-}
+
 
